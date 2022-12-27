264e4ad71fe6eda2c1100aaaa4d45a210c020f49
Reuben Firmin <rf@flavor8.com>
2022-12-27 16:30:20 -0600

    multiple camera streams

--- streams.sh
+++ streams.sh
@@ -2,5 +2,8 @@
 
 . ./diycam.config
 
-echo "ffmpeg -f v4l2 -framerate 60 -video_size 1280x720 -input_format mjpeg -i /dev/video0 -f segment -segment_time 300 -segment_list ${monitor_dir}/segments.txt ${monitor_dir}/trig%05d.mp4"
-ffmpeg -f v4l2 -framerate 60 -video_size 1280x720 -input_format mjpeg -i /dev/video0 -f segment -segment_time 300 -segment_list ${monitor_dir}/segments.txt ${monitor_dir}/trig%05d.mp4
+# for each declared camera, record the video stream and write segments
+for camera in ${!cameras[@]}; do
+	camera_ip=${cameras[$camera]}
+	ffmpeg -hwaccel cuda -rtsp_transport tcp -i rtsp://$camera_creds@$camera_ip/video/1 -map 0 -c:v libx264 -reset_timestamps 1 -f segment -segment_time 300 -segment_list ${monitor_dir}/segments$camera.txt &
+done
