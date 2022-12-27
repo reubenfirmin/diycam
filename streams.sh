a21e6d061d4114b14d972c9deaab311808e074a2
Reuben Firmin <rf@flavor8.com>
2022-12-27 16:57:23 -0600

    better ffmpeg commands

--- streams.sh
+++ streams.sh
@@ -5,5 +5,14 @@
 # for each declared camera, record the video stream and write segments
 for camera in ${!cameras[@]}; do
 	camera_ip=${cameras[$camera]}
-	ffmpeg -hwaccel cuda -rtsp_transport tcp -i rtsp://$camera_creds@$camera_ip/video/1 -map 0 -c:v libx264 -reset_timestamps 1 -f segment -segment_time 300 -segment_list ${monitor_dir}/segments$camera.txt &
+	keep_alive camera &
 done
+
+# ffmpeg can die if there is corruption from the camera; if that happens, restart it
+keep_alive() {
+	camera=$1
+        camera_ip=${cameras[$camera]}
+	while true; do
+		ffmpeg -rtsp_transport tcp -i rtsp://$camera_creds@$camera_ip/video/1 -map 0 -c:v h264 -preset:v ultrafast -reset_timestamps 1 -f segment -segment_time 300 -strftime 1 -segment_list ${monitor_dir}/segments$camera.txt cam${camera}_out%Y%m%d_%H%M%S.mp4
+	done
+}
